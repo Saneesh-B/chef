f7bdbbae2b809eae516c362a52169cd8b4ffacf3
Saneesh <b_saneesh@live.com>
2017-01-24 11:15:06 +0530

    First

---
+++ nginx/metadata.rb
@@ -0,0 +1,9 @@
+name             'nginx'
+maintainer       'Metron'
+maintainer_email 'something@metronconsulting.com'
+license          'All rights reserved'
+description      'Installs/Configures nginx'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "apt"
